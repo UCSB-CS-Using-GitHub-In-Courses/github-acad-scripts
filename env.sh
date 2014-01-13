370f2b1f5b31cd5ac0f8567c4b4c78634697a3ed
pconrad <pconrad@cs.ucsb.edu>
2014-01-12 23:17:36 -0800

    created env.sh.sample

--- env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export GHA_GITHUB_ORG=UCSB-CS56-W14
-export GHA_STUDENT_LIST_URL=https://docs.google.com/spreadsheet/pub?key=0Ai3r-rcTtDnidHd4M1dyRzZZM05MM3NEMzBfSHdYY1E
-export GHA_PAIR_LIST_URL=https://docs.google.com/spreadsheet/ccc?key=xxxxx
-export GHA_STARTPOINT_DIR=../CS56-W14-Labs
-export GHA_WORKDIR=~/cs56-W14-gha-work
